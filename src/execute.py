a1a2782596c813c7bb952e9147e65bf1dc48ffb9
Dennis Urtubia <dennisurtubia@alunos.utfpr.edu.br>
2021-05-17 20:42:25 -0300

    feat(scripts): Adds script to extract license informations
    Script that:
    - Download repositories from csv
    - Iterates over the repositories by running LicenseFinder
    - Dump the result of LicenseFinder execution in a file
    
    Co-authored-by: Otávio Goes <otavio@ogoes.dev>

---
+++ src/execute.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import os
+import csv
+import json
+from pprint import pprint
+
+with open('src/repositories.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    repositories = [(name.split('/')[1], url) for [name, _, _, language, _, url, _, _] in readCSV if language == 'JavaScript']
+
+if not os.path.exists('repositories'):
+    os.makedirs('repositories')
+
+projects = {}
+
+for (name, url) in repositories:
+    path = f'repositories/{name}'
+
+    os.system(f'[ -d {path} ] || git clone {url} {path}')
+    os.system(f'cd {path} && dlf "license_finder report --format json --prepare --save=license_finder_report.json" && cd ..')
+
+    with open(f'{path}/license_finder_report.json') as jsonfile:
+        projects[name] = json.loads(jsonfile.read())
+
+    os.system(f'rm -rf {path}')
+
+with open('./projects.json', 'w') as jsonfile:
+    json.dump(projects, jsonfile)
